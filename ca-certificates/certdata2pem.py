4a154df1fa54e3f85749823cd30d57258c6c1480
Kimpe Andy <andykimpe@gmail.com>
2024-06-20 18:15:10 +0200

    Update certdata2pem.py

--- ca-certificates/certdata2pem.py
+++ ca-certificates/certdata2pem.py
@@ -376,3 +376,5 @@ for tobj in objects:
             f.write("\n\n")
         f.close()
         print " -> written as '%s', trust = %s, openssl-trust = %s, distrust = %s, openssl-distrust = %s" % (fname, trustbits, openssl_trustflags, distrustbits, openssl_distrustflags)
+
+
